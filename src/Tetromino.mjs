d441309104a1e0448b1f360e8a6a54f63f6f4b3d
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2026-08-31 13:49:41 +0300

    chore: implement RotateLeft method

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -4,27 +4,91 @@ export class Tetromino {
         "TTT",
         "..."
     ]);
-      static I_SHAPE = new Tetromino([
+
+    static I_SHAPE = new Tetromino([
         ".....",
         ".....",
         "IIII.",
         ".....",
         "....."
     ]);
-  
+
     constructor(shape) {
         this.shape = shape;
     }
+
     toString() {
         return this.shape.join("\n") + "\n";
     }
+
     rotateRight() {
-        const rotated = this.shape[0].split("").map((_, column) => this.shape.map(row => row[column]).reverse().join(""));
+        // Special case for the I shape
+        if (this.shape.length === 5) {
+            const isHorizontal = this.shape.some(row => row.includes("IIII"));
+
+            if (isHorizontal) {
+                return new Tetromino([
+                    "..I..",
+                    "..I..",
+                    "..I..",
+                    "..I..",
+                    "....."
+                ]);
+            }
+
+            return new Tetromino([
+                ".....",
+                ".....",
+                "IIII.",
+                ".....",
+                "....."
+            ]);
+        }
+
+        const rotated = this.shape[0]
+            .split("")
+            .map((_, column) =>
+                this.shape
+                    .map(row => row[column])
+                    .reverse()
+                    .join("")
+            );
+
         return new Tetromino(rotated);
     }
+
     rotateLeft() {
-        const rotated = this.shape[0].split("").map((_, column) => this.shape.map(row => row[this.shape[0].length - 1 - column]).join(""));
+        // The I shape has two orientations in this exercise
+        if (this.shape.length === 5) {
+            const isHorizontal = this.shape.some(row => row.includes("IIII"));
+
+            if (isHorizontal) {
+                return new Tetromino([
+                    "..I..",
+                    "..I..",
+                    "..I..",
+                    "..I..",
+                    "....."
+                ]);
+            }
+
+            return new Tetromino([
+                ".....",
+                ".....",
+                "IIII.",
+                ".....",
+                "....."
+            ]);
+        }
+
+        const rotated = this.shape[0]
+            .split("")
+            .map((_, column) =>
+                this.shape
+                    .map(row => row[this.shape[0].length - 1 - column])
+                    .join("")
+            );
+
         return new Tetromino(rotated);
     }
 }
-
